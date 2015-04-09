e63de77deeac2a524f43cf205f1d14a0b7de4005
James Dixon <jim.w.dixon@gmail.com>
2015-04-09 17:36:26 -0600

    another update

--- index.js
+++ index.js
@@ -3,4 +3,15 @@
 
 module.exports = {
   name: 'ember-cli-simple-auth-firebase'
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
+  included: function(app) {
+    this._super.included(app);
+    this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.js');
+    this.app.import(app.bowerDirectory + '/firebase/firebase.js');
+    this.app.import(app.bowerDirectory + '/firebase-es6-shim/firebase-es6-shim.js');
+  }
 };

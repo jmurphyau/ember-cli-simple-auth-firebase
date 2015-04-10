5717bf8f04355fb50dfeceaf060cb2f52af9e99d
James Dixon <jim.w.dixon@gmail.com>
2015-04-09 19:38:48 -0600

    adasd

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ module.exports = {
     this._super.included(app);
     this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.js');
     this.app.import(app.bowerDirectory + '/firebase/firebase.js');
-    this.app.import('/vendor/firebase-es6-shim/firebase-es6-shim.js');
+    this.app.import('/vendor/firebase-es6-shim/firebase-es6-shim.js', {
+    	type: 'vendor',
+    	exports: { 'firebase': ['default'] }
+    });
   }
 };
